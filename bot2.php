221f2841e0afa3c919aa0350aa4a935f95299c17
chunmusic <32039077+chunmusic@users.noreply.github.com>
2017-09-18 14:55:58 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -1,6 +1,6 @@
 $composer require linecorp/line-bot-sdk
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<8GlfmVUKnprI71gm4b7UmgsAGk+uGX8Osq3Q+NJAqRG2Ogr0Zkpgc42TTrERy+eQHKpDcljzll+B/QjKgT+laG1IBmWzkpPRGM3bxOYUOSUmg//COK4vasfqJTlQ1P7WEFmgpLDzjbvR3Qiop9JMbQdB04t89/1O/w1cDnyilFU=>');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 
 $response = $bot->replyText('<reply token>', 'hello!');
