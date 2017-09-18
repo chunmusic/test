ece592fb64a5441b18417a189741dc9aa24873b7
chunmusic <32039077+chunmusic@users.noreply.github.com>
2017-09-18 22:53:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,8 +32,8 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				'messages' => [$messages2]
+				'messages' => [$messages],[$messages2],
+				 
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
