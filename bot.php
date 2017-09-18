bc98bbebfde9aee3eefedc5a593b4be033f03d23
chunmusic <32039077+chunmusic@users.noreply.github.com>
2017-09-18 22:43:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,13 +21,19 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-
+			
+			// Build another message to reply back
+			$messages2 = [
+				'type' => 'text',
+				'text' => "Hello, World"
+			];
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
-				'messages' => "test"
+				'messages2' => [$messages2]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
