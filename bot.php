27782dcdb394692650b805cb100261c60a5ca050
chunmusic <32039077+chunmusic@users.noreply.github.com>
2017-09-18 15:10:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,3 +45,23 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+
+
+{
+  "events": [
+      {
+        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+             "type": "user",
+             "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+         },
+         "message": {
+             "id": "325708",
+             "type": "text",
+             "text": "Hello, world"
+          }
+      }
+  ]
+}
